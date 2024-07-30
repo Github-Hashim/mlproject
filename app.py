e1329edbc19ebed870f2280fa83118029467111f
Muhammad Hashim <muhammadhashim22.5.2001@gmail.com>
2024-07-30 23:29:58 +0500

    Data Transformation

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from src.mlproject.logger import logging
 import sys
+from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
-
+from src.mlproject.components.data_transformation import DataTransformation
 if __name__ == "__main__":
+    logging.info("The execution has started.")
     try:
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
+        data_tranformation = DataTransformation()
+        train_arr, test_arr,_ = data_tranformation.initiate_data_transformation(train_data_path, test_data_path)
+        print(train_arr, test_arr)
     except Exception as e:
         raise CustomException(e, sys)
--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -4,7 +4,7 @@ from src.mlproject.exception import CustomException
 import os, sys
 from src.mlproject.utils import read_sql_data
 from sklearn.model_selection import train_test_split
-
+import pandas as pd
 @dataclass
 class DataIngestionConfig:
     train_data_path:str = os.path.join('artifacts', 'train.csv')
@@ -18,7 +18,8 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         try:
             ## reading the data from mysql
-            df = read_sql_data()
+            #df = read_sql_data()
+            df = pd.read_csv(os.path.join('notebook/data','raw.csv'))
             logging.info("Reading completed from mysql database.")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
