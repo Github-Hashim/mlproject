32f812a0b6bf5698fbab5b1ba06f0a3486584d57
Muhammad Hashim <muhammadhashim22.5.2001@gmail.com>
2024-08-01 00:59:58 +0500

    Model trainer update

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-import sys
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
-from src.mlproject.components.data_ingestion import DataIngestion
-from src.mlproject.components.data_transformation import DataTransformation
-if __name__ == "__main__":
-    logging.info("The execution has started.")
-    try:
-        data_ingestion = DataIngestion()
-        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
 
-        data_tranformation = DataTransformation()
-        train_arr, test_arr,_ = data_tranformation.initiate_data_transformation(train_data_path, test_data_path)
-        print(train_arr, test_arr)
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
+from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+
+import sys
+
+from src.mlproject.components.model_trainer import ModelTrainer
+if __name__=="__main__":
+    logging.info("The execution has started")
+
+    try:
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+        ## Model Training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        
     except Exception as e:
-        raise CustomException(e, sys)
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
