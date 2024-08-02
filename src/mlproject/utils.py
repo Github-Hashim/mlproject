a756a1f8a0d844f33f07166fde73bf503f024132
Muhammad Hashim <muhammadhashim22.5.2001@gmail.com>
2024-08-02 22:50:56 +0500

    Deployment Issues Solved

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -18,6 +18,15 @@ user=os.getenv("user")
 password=os.getenv("password")
 db=os.getenv('db')
 
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
 
 
 def read_sql_data():
